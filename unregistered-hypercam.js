dd7d5e715fd7361a877dd54d64a48922c5b7aa8c
ryan.beaumont <ryan.beaumont@voyager.nz>
2023-05-09 18:26:47 +1200

    update to use hosted

--- unregistered-hypercam.js
+++ unregistered-hypercam.js
@@ -1,12 +1,15 @@
 var img = document.createElement("img");
-img.src = "./unregistered-hypercam.png";
+img.src =
+  "https://github.com/RyBeau/UnregistedHyperCam/blob/main/unregistered-hypercam.png?raw=true";
 img.style.position = "fixed";
 img.style.top = "0";
 img.style.left = "0";
 img.height = "30";
 document.body.appendChild(img);
 
-var audio = new Audio("./unregistered-hypercam.mp3");
+var audio = new Audio(
+  "https://github.com/RyBeau/UnregistedHyperCam/raw/main/unregistered-hypercam.mp3"
+);
 audio.loop = true;
 
 audio.addEventListener("canplaythrough", () => {
