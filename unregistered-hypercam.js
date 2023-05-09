837a1f77ab69a6875dc5693281ddf06dd49111ea
ryan.beaumont <ryan.beaumont@voyager.nz>
2023-05-09 18:38:08 +1200

    fix z index

--- unregistered-hypercam.js
+++ unregistered-hypercam.js
@@ -1,14 +1,15 @@
 var img = document.createElement("img");
 img.src =
-  "https://github.com/RyBeau/UnregistedHyperCam/blob/main/unregistered-hypercam.png?raw=true";
+  "https://cdn.jsdelivr.net/gh/RyBeau/UnregisteredHyperCam@main/unregistered-hypercam.png";
 img.style.position = "fixed";
 img.style.top = "0";
 img.style.left = "0";
 img.height = "30";
+img.zIndex = "999";
 document.body.appendChild(img);
 
 var audio = new Audio(
-  "https://github.com/RyBeau/UnregistedHyperCam/raw/main/unregistered-hypercam.mp3"
+  "https://cdn.jsdelivr.net/gh/RyBeau/UnregisteredHyperCam@main/unregistered-hypercam.mp3"
 );
 audio.loop = true;
 
